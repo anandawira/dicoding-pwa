2b383442a81b11d113ccfbe7d263ca221d5a064d
Ananda Wiradharma <anandawira754@gmail.com>
2020-10-04 14:55:16 +0700

    add service-worker and manifest.json

---
+++ submission-1/service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "cssframeworks";
+var urlsToCache = [
+  "/",
+  "/nav.html",
+  "/index.html",
+  "/pages/home.html",
+  "/pages/bootstrap.html",
+  "/pages/bulma.html",
+  "/pages/materialize.html",
+  "/pages/semantic-ui.html",
+  "/css/materialize.min.css",
+  "/js/materialize.min.js",
+  "/assets/Bootstrap.svg",
+  "/assets/bulma-logo.png",
+  "/assets/materialize.svg",
+  "/assets/semantic-ui.png",
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches
+      .match(event.request, { cacheName: CACHE_NAME })
+      .then(function (response) {
+        if (response) {
+          console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
+          return response;
+        }
+
+        console.log(
+          "ServiceWorker: Memuat aset dari server: ",
+          event.request.url
+        );
+        return fetch(event.request);
+      })
+  );
+});
