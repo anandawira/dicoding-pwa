4db6fe6be6d4c8e4896852805d044a7586df4101
Ananda Wiradharma <anandawira754@gmail.com>
2020-10-07 13:58:27 +0700

    nothing lul

---
+++ latihanpwa1/sw.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = "codepolitan-reader-lite-v1";
+var urlsToCache = ["/", "/index.html", "/js/main.js", "/img/logo.png"];
+
+self.addEventListener("install", function (event) {
+  console.log("ServiceWorker: Menginstall..");
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("ServiceWorker: Membuka cache..");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+elf.addEventListener("activate", function (event) {
+  console.log("Aktivasi service worker baru");
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (
+            cacheName !== CACHE_NAME &&
+            cacheName.startsWith("codepolitan-reader-lite")
+          ) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches
+      .match(event.request, { cacheName: CACHE_NAME })
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        var fetchRequest = event.request.clone();
+        return fetch(fetchRequest).then(function (response) {
+          if (!response || response.status !== 200) {
+            return response;
+          }
+          var responseToCache = response.clone();
+          caches.open(CACHE_NAME).then(function (cache) {
+            cache.put(event.request, responseToCache);
+          });
+          return response;
+        });
+      })
+  );
+});
